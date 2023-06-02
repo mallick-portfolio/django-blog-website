d18124fd35f5a3217f7fe283d541db89b1c3a7b5
mallick-portfolio <tamal1714019@gmail.com>
2023-06-03 00:40:28 +0600

    added single blog details

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, blogs
+from .views import home, blogs, blogDetails
 
 urlpatterns = [
     path("", home, name='home'),
     path("blogs/", blogs, name='blogs'),
+    path("blogs/<int:pk>/", blogDetails, name='blog-details'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,16 +1,42 @@
 from django.shortcuts import render
-from .models import Blog
+from .models import Blog, Category, Tag
 # Create your views here.
 
 
 def home(request):
     #  blogs = Blog.objects.order_by('-created_at')
     blogs = Blog.objects.order_by('-created_date')[:3]
+    categories = Category.objects.all()
+    tags = Tag.objects.all()[:9]
     context = {
-        'blogs': blogs
+        'blogs': blogs,
+        'categories': categories,
+        'tags': tags,
     }
     return render(request, 'home.html', context)
 
 
 def blogs(request):
-    return render(request, 'blogs.html')
+    blogs = Blog.objects.order_by('-created_date')[:3]
+    categories = Category.objects.all()
+    tags = Tag.objects.all()[:9]
+    context = {
+        'blogs': blogs,
+        'categories': categories,
+        'tags': tags,
+    }
+    return render(request, 'blogs.html', context)
+
+
+def blogDetails(request, pk):
+    blogs = Blog.objects.order_by('-created_date')[:3]
+    blog = Blog.objects.filter(pk=pk).first()
+    categories = Category.objects.all()
+    tags = Tag.objects.all()[:9]
+    context = {
+        'blog': blog,
+        'blogs': blogs,
+        'categories': categories,
+        'tags': tags,
+    }
+    return render(request, 'post-details.html', context)
